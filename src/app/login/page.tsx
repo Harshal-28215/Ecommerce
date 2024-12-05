748499c20065b5de84bb6600af817a1c1038a109
Hr_28215 <harshalborad2003@gmail.com>
2024-12-05 14:59:07 +0530

    feat: add login and signup forms with validation and password input toggle

---
+++ src/app/login/page.tsx
@@ -0,0 +1,10 @@
+import LoginForm from '@/components/Authentications/LoginForm'
+import React from 'react'
+
+function page() {
+  return (
+    <LoginForm />
+  )
+}
+
+export default page
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,10 @@
+import SignupForm from '@/components/Authentications/SignupForm'
+import React from 'react'
+
+function page() {
+  return (
+    <SignupForm />
+  )
+}
+
+export default page
