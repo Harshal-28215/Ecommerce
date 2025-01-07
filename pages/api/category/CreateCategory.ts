aa2147bc3a2fc2f003d463eacc1181a5fa7851f3
Hr_28215 <harshalborad2003@gmail.com>
2025-01-07 18:57:09 +0530

    feat: add category update functionality with authentication and authorization

--- pages/api/category/CreateCategory.ts
+++ pages/api/category/CreateCategory.ts
@@ -2,26 +2,29 @@ import connectToDatabase from "@/lib/mongodb/db";
 import Category from "@/lib/schemas/Category";
 import getCategory from "@/utils/Routes/categoryRoutes/GET";
 import createCategory from "@/utils/Routes/categoryRoutes/POST";
+import categoryUpdate from "@/utils/Routes/categoryRoutes/PUT";
 import { NextApiRequest, NextApiResponse } from "next";
 
 const CreateCategory = async (req: NextApiRequest, res: NextApiResponse) => {
     await connectToDatabase();
     if (req.method === 'GET') {
+
         // Get all categories or categories with id
         await getCategory(req, res);
+
     } else if (req.method === 'POST') {
+
         // Create a new category
         createCategory(req, res);
+
     } else if (req.method === 'PUT') {
+
         // Update a category
-        console.log(req.body);
-        
-        const updateCategory = await Category.findByIdAndUpdate(req.query.id, req.body, { new: true });
-        res.status(200).json(updateCategory);
+        await categoryUpdate(req, res);
+
     }
     else {
-        // Handle unsupported methods
-        res.setHeader('Allow', ['GET', 'POST']); // Specify allowed methods
+        res.setHeader('Allow', ['GET', 'POST', 'PUT']);
         res.status(405).json({ error: `Method ${req.method} not allowed` });
     }
 }
---
+++ src/utils/Routes/categoryRoutes/PUT.ts
@@ -0,0 +1,21 @@
+import Category from "@/lib/schemas/Category";
+import authenticate from "@/utils/Middleware/authentication";
+import authorize from "@/utils/Middleware/authorization";
+import { NextApiRequest, NextApiResponse } from "next";
+
+
+export default async function categoryUpdate(req: NextApiRequest, res: NextApiResponse) {
+
+    const user = authenticate(req, res);
+    if (!user) return res.status(401).json({ message: "Not authenticated" });
+
+    const cheackauth = authorize(["admin"], Category);
+    if (!cheackauth) return res.status(403).json({ message: "Unauthorized" });
+
+    try {
+        const updateCategory = await Category.findByIdAndUpdate(req.query.id, req.body, { new: true });
+        res.status(200).json(updateCategory);
+    } catch (error) {
+        res.status(400).json({ message: "Error updating category" });
+    }
+}
